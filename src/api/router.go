ae41ae34df974e217f3599d57009609e7c996c34
e <707425690@qq.com>
2018-05-05 14:56:35 +0800

    完成路由配置，文件框架完成

--- src/api/router.go
+++ src/api/router.go
@@ -11,10 +11,31 @@ func routerInit(){
 	v1 := config.service.router.Group("v1")
 	{
 		//stu 对接apihdu
-		v1.GET("/user/logout", func(c *gin.Context) {
+		v1.GET("/user/login", func(c *gin.Context) {
 			token := c.Query("token")
 			fmt.Println(token)
 		})		
+		v1.GET("/user/logout",func(c *gin.Context){
+			
+		})		
+		v1.GET("/user/info",func(c *gin.Context){
+			
+		})		
+		v1.GET("/user/token/logout",func(c *gin.Context){
+			
+		})		
+		v1.GET("/user/regisitor",func(c *gin.Context){
+			
+		})		
+		v1.GET("/mp3/serch",func(c *gin.Context){
+			
+		})		
+		v1.GET("/user/downloadLink",func(c *gin.Context){
+			
+		})		
+		v1.POST("/user/downloadLink",func(c *gin.Context){
+			
+		})
 
 	}
 	config.service.router.Run(":8081")
