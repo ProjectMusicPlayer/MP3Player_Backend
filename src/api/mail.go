c3b971bd6932f563d89fe6c58de7aa6df43fc33e
e <707425690@qq.com>
2018-05-21 12:20:59 +0800

    增加一个邮件接口

--- src/api/mail.go
+++ src/api/mail.go
@@ -17,7 +17,25 @@ func sendmail(to,user,state string)(error){
 	req := curl.NewRequest()
 	//https://api.mp3.h-00.com/v1/user/regisitor/mailRedirect/
     resp, err := req.
-        SetUrl("**********************").
+        SetUrl("https://projm.billsjc.com/index.php/console/user_sendmail?addr="+to+"&user="+user+"&&url=https%3a%2f%2fapi.mp3.h-00.com%2fv1%2fuser%2fregisitor%2fmailRedirect%2f"+state).
+        Post()
+
+    if err != nil {
+        return err
+    } else {
+        if resp.IsOk() {
+			fmt.Print(resp.Body)
+			return nil
+		}else{
+			return fmt.Errorf("send smail failed")
+		}
+	}
+}
+func sendmailf(to,user,state string)(error){
+	req := curl.NewRequest()
+	//https://api.mp3.h-00.com/v1/user/regisitor/mailRedirect/
+    resp, err := req.
+        SetUrl("https://projm.billsjc.com/index.php/console/user_sendmail2?addr="+to+"&user="+user+"&&url=https%3a%2f%2fapi.mp3.h-00.com%2fv1%2fuser%2fregisitor%2fmailRedirect%2f"+state).
         Post()
 
     if err != nil {
