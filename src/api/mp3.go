afa1141ae861e99e7bbf73cfa310e2ea319e0a7e
e <707425690@qq.com>
2018-05-21 12:21:12 +0800

    添加mp3处理方法

--- src/api/mp3.go
+++ src/api/mp3.go
@@ -1,5 +1,18 @@
 package main
 
-import(
-	
+import (
+	"strconv"
 )
+
+func addMp3(name string,singer string,books string,len string,mp3addr string,lrcaddr string)(int,interface{}){
+	len6,err := strconv.Atoi(len)
+	leni := int64(len6)
+	if(err!=nil){
+		return makeErrJson(403,40300,err)
+	}
+	err = addMp3DB(name,singer,books,leni,mp3addr,lrcaddr)
+	if(err!=nil){
+		return makeErrJson(403,40301,err)
+	}
+	return makeSuccessJson("add mp3 success")
+}
